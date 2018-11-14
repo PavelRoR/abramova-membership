a469fe1ac426df724aab0741fe5d6f328be94287
pavelp21 <pavelvp2182@yandex.ru>
2018-11-14 23:17:11 +0500

    Section Prices is done

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
 
-
+    /* Якорь */
+    $(function () {
+        $("a[href='#prices']").click(function (h) {
+            h.preventDefault();
+            var f = $(this).attr("href"),
+                g = $(f).offset().top;
+            $("body,html").animate({
+                scrollTop: g
+            }, 1500)
+        });
+    });
 
 /*Конец документа*/
 });
