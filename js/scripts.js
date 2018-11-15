656193013c5b01ec8b2dcfc2d08691679bda7939
pavelp21 <pavelvp2182@yandex.ru>
2018-11-15 19:57:16 +0500

    Section About is added and adepted

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,21 @@ $(document).ready(function() {
             }, 1500)
         });
     });
-
+    /* Видео */
+	$(".video_wrapper img").click(function () {
+		var a = $(this).parent().attr("data-youtube");
+		$(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1" allowfullscreen></iframe>')
+    });
+    /* Отзывы */
+	$('#reviews').owlCarousel({
+		loop: true,
+		margin: 20,
+		nav: true,
+		navText: ["", ""],
+		dots: false,
+		items: 1,
+		autoHeightClass: 'owl-height',
+		autoHeight: true
+	})
 /*Конец документа*/
 });
